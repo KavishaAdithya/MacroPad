2d53f8dddb47b5550856a844b37ed762d633dac6
Kavisha Adithya <kavishalankarathna7@gmail.com>
2025-03-01 20:16:25 +0530

    changed the functionality, fixed the url printing error

--- Macropad/Macropad.ino
+++ Macropad/Macropad.ino
@@ -1,163 +1,173 @@
 #include <BleKeyboard.h>
 
+#define CLK 13
+#define DT 4
+#define Browser_pin 14
+#define Youtube 27
+#define Github 26
 
-#define CLK_PIN 13
-#define DT_PIN 4
+int16_t inputDelta = 0;   
 
-#define DIRECTION_CW 0
-#define DIRECTION_CCW 1
-
-int counter = 0;
-int direction  = DIRECTION_CW;
-int CLK_state;
-int prev_CLK_state;
+int button_state = HIGH;
+int lastButtonState = HIGH;          // Counts up or down depending which way the encoder is turned
+static uint8_t state = 0;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;
 
 
 BleKeyboard bleKeyboard;
 
-bool buttonStateYT = false;
-bool buttonStateGT = false;
-
-
 void setup() {
   Serial.begin(115200);
-  
- 
   bleKeyboard.begin();
  
-  pinMode(5,INPUT_PULLUP); // OPen YT
-  pinMode(18,INPUT_PULLUP); // Open Github
-  pinMode(CLK_PIN, INPUT_PULLUP);
-  pinMode(DT_PIN,INPUT_PULLUP);
-  
-  prev_CLK_state = digitalRead(CLK_PIN);
+  pinMode(CLK, INPUT_PULLUP);
+  pinMode(DT,INPUT_PULLUP);
+  pinMode(Browser_pin,INPUT_PULLUP);
+  pinMode(Youtube,INPUT_PULLUP);
+  pinMode(Github, INPUT_PULLUP);
 
 }
+
 
 void loop() {
-
-// Volume Control
-
-
-
-if(bleKeyboard.isConnected()) {
-
-
-  CLK_state = digitalRead(CLK_PIN);
-
-
-  if (CLK_state != prev_CLK_state && CLK_state == HIGH){
-    if (digitalRead(DT_PIN) == HIGH){
-
-      delay(50);
-
-      if (digitalRead(DT_PIN) == HIGH){
-      counter--;
-      direction = DIRECTION_CCW; 
-      bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
-      // Serial.println("Volume Down");
-
-      }
-
-    }
-    if (digitalRead(DT_PIN) == LOW){
-
-      delay(50);
-      
-      if (digitalRead(DT_PIN) == LOW){
-
-      
-      counter++;
-      direction = DIRECTION_CW; 
-      bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
-      // Serial.println("Volume Up");
-
-      }
-
-    }
-
-  }
-
-Serial.println(counter);
-prev_CLK_state = CLK_state;
-
-// Open Youtube
-if (digitalRead(5) == LOW){
-  delay(50);
-
-if (digitalRead(5) == LOW){
   
+ if(bleKeyboard.isConnected()) {
 
-  buttonStateYT = !buttonStateYT;
+  readEncoder();    // Volume Control 
 
-if (buttonStateYT){
+  if (digitalRead(Browser_pin) == 0){
+    delay(100);
+    if (digitalRead(Browser_pin) == 0){
+    bleKeyboard.write(KEY_MEDIA_WWW_HOME);
+    Serial.write("Chrome");
+    }  
+  }
 
-bleKeyboard.press(KEY_LEFT_GUI);
-bleKeyboard.print("r");
-bleKeyboard.releaseAll();
+  // if(debounce_delay(Youtube)){
 
-delay(500);
-bleKeyboard.print("www.youtube.com");
+  //   Open_Website("https://www.youtube.com");
 
-bleKeyboard.press(KEY_RETURN);
-bleKeyboard.releaseAll();
-Serial.println("Youtube");
-}
+  // }
 
-}
+  if(debounce_delay(Github)){
 
-}
-
-if(digitalRead(18) == LOW){
-  delay(50);
-
-if (digitalRead(18) == LOW){
-  buttonStateGT = !buttonStateGT;
-
-if (buttonStateGT){
-
-bleKeyboard.press(KEY_LEFT_GUI);
-bleKeyboard.print("r");
-bleKeyboard.releaseAll();
-
-delay(500);
-bleKeyboard.print("www.github.com");
-
-bleKeyboard.press(KEY_RETURN);
-
-bleKeyboard.releaseAll();
-Serial.println("Github");
-
-}
-
-}
-
-}
-
+    Open_Website("https://github.com/");
 
   }
+
+
+}
 
  else{
     Serial.println("NOT CONNECTED");
     delay(1000);
   }
 
+}
 
 
+void readEncoder() {
+    
+    bool CLKstate = digitalRead(CLK);
+    bool DTstate = digitalRead(DT);
+
+    switch (state) {
+        case 0:                         // Idle state, encoder not turning
+            if (!CLKstate){             // Turn clockwise and CLK goes low first
+                state = 1;
+            } else if (!DTstate) {      // Turn anticlockwise and DT goes low first
+                state = 4;
+            }
+            break;
+        // Clockwise rotation
+        case 1:                     
+            if (!DTstate) {             // Continue clockwise and DT will go low after CLK
+                state = 2;
+            } 
+            break;
+        case 2:
+            if (CLKstate) {             // Turn further and CLK will go high first
+                state = 3;
+            }
+            break;
+        case 3:
+            if (CLKstate && DTstate) {  // Both CLK and DT now high as the encoder completes one step clockwise
+                state = 0;
+                bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+                
+            }
+            break;
+        // Anticlockwise rotation
+        case 4:                         // As for clockwise but with CLK and DT reversed
+            if (!CLKstate) {
+                state = 5;
+            }
+            break;
+        case 5:
+            if (DTstate) {
+                state = 6;
+            }
+            break;
+        case 6:
+            if (CLKstate && DTstate) {
+                state = 0;
+                bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+            }
+            break; 
+    }
+
+  
+}
 
 
+bool debounce_delay(int button_pin){
 
 
+  int reading = digitalRead(button_pin);
 
 
+  if (reading != lastButtonState){
+  lastDebounceTime = millis();
+  }
+
+  if((millis() - lastDebounceTime)>debounceDelay){
+
+  if(reading!=button_state){
+    button_state = reading;
+
+    if(button_state == LOW){
+      lastButtonState = reading;
+      return true;
+    }
+
+   
+    }
+  }
+
+  lastButtonState = reading;
+  return false;
+}
 
 
+void Open_Website (String url){
 
+  bleKeyboard.press(KEY_LEFT_GUI);
+    bleKeyboard.print("r");
+    delay(100);
+    bleKeyboard.releaseAll();
+    delay(200);
 
-
-
-
-
+    for(int i = 0; i < url.length(); i++) {
+      bleKeyboard.print(url[i]);
+      delay(10); // Small delay between characters
+    }
+    delay(2000);
+    bleKeyboard.press(KEY_RETURN);
+    delay(300);
+    bleKeyboard.releaseAll();
 
 
 }
+
+
